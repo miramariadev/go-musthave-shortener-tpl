4dfe3488a86b91555b3e9ea84c885bf03eb5ee0d
m.kryuchkova <m.kryuchkova@studionx.ru>
2022-01-09 19:45:04 +0300

    edit test & add const for localhost

--- internal/app/url_shortener_handler.go
+++ internal/app/url_shortener_handler.go
@@ -2,10 +2,11 @@ package app
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 const host = "http://localhost:8080/"
@@ -47,10 +48,14 @@ func (c *URLShortenerHandler) HandleShortURL(w http.ResponseWriter, r *http.Requ
 		return
 
 	case "GET":
-		data := strings.Split(r.URL.String(), "/")
-		id := data[len(data)-1]
+		id := chi.URLParam(r, "urlID")
+		if id == "" {
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
 
 		longURL, err := c.service.GetLongURLByID(id)
+		log.Println(id)
 		if err != nil {
 			http.Error(w, "Not found", http.StatusNotFound)
 			return
@@ -66,3 +71,6 @@ func (c *URLShortenerHandler) HandleShortURL(w http.ResponseWriter, r *http.Requ
 		return
 	}
 }
+
+//curl -X POST -H "Content-Type: text/plain" -d  'http://rp21sh.yandex/avshz7qrl/h4ululwnp7bow' http://localhost:8080
+//curl -X GET http://localhost:8080/56430
