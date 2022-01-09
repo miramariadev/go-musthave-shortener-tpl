7f5fd9ac27378f7ad86ad26775345772a21b1916
m.kryuchkova <m.kryuchkova@studionx.ru>
2022-01-09 20:43:50 +0300

    Revert "test"
    This reverts commit a04275113336d6cf7905958b0a982ab5bcebc678.

--- internal/app/url_shortener_handler.go
+++ internal/app/url_shortener_handler.go
@@ -46,11 +46,6 @@ func (c *URLShortenerHandler) HandleShortURL(w http.ResponseWriter, r *http.Requ
 			return
 		}
 
-		/*data := strings.Split(r.URL.String(), "/")
-		id = data[len(data) - 1]*/
-
-		//id это символы после слеша
-
 		longURL, err := c.service.GetLongURLByID(id)
 		if err != nil {
 			http.NotFoundHandler()
